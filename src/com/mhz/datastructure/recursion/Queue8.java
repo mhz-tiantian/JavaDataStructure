d625c955fb14c6e6cbfe2858d343fc86a043841e
mahaizhen <229358084@qq.com>
2020-04-14 14:48:46 +0800

    8皇后的解法 问题

--- src/com/mhz/datastructure/recursion/Queue8.java
+++ src/com/mhz/datastructure/recursion/Queue8.java
@@ -27,18 +27,65 @@ public class Queue8 {
     int max = 8;
     // 定义一个数组, 保存皇后放置位置的结果  比如 arr={0,4,7,5,2,6,1,3}
     int[] array = new int[8];
+    static int count = 0;
+
+
+    public static void main(String[] args) {
+        Queue8 queue8 = new Queue8();
+        queue8.check(0);
+        System.out.printf("一总有%d解法", count);
+    }
+
+    //编写一个方法, 放置第n个皇后
+    // 注意:check是第一次递归时, 进入到check中都有
+    // for(int i=0;i<max;i++) 因此会有回溯
+    private void check(int n) {
+        if (n == max) {
+            // 如果n==8的時候， 表示第8個皇后已经放好了.  直接打印返回
+            print();
+            return;
+        }
+
+        for (int i = 0; i < max; i++) {
+            // 先把当前这个皇后n 放在该行的第1列
+            array[n] = i;
+            // 判断是否冲突, 如果不冲突 接着放n+1个皇后, 开始递归
+            if (judge(n)) {
+                check(n + 1);
+            }
+            // 如果冲突, 就继续执行array[n]=i;就是第n个皇后,放置在本行得  后移一个位置
+        }
+
+    }
+
+    private void print() {
+        count++;
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "\t");
+        }
+        System.out.println();
+
+    }
 
 
     /**
      * 查看当我们放置第n个皇后, 就去检测该皇后是否和前面已经摆放的皇后冲突
      *
      * @param n 表示第n个皇后
+     * @return false 表示冲突, true表示不冲突
      */
-    private void judge(int n) {
-
+    private boolean judge(int n) {
         for (int i = 0; i < n; i++) {
+            // 1.array[i]==array[n]表示判断第n个皇后是否和前面的n-1个皇后在同一列
+            // Math.abs(int ) 返回int的绝对值
+            // 2. Math.abs(n-i)==Math.abs(array[n]-array[i]) 判断第n个皇后,是否和第i皇后是否在同一个斜线上
 
+            // 3. 判断是否在同一行 , 没有必要, n每次都在递增
+            if (array[i] == array[n] || (Math.abs(n - i) == Math.abs(array[n] - array[i]))) {
+                return false;
+            }
         }
+        return true;
 
     }
 
